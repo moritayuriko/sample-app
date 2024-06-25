e00bd924a83707f412fe3df3c349d51c2b179972
moritayuriko <yuriko-morita@infinityloop.co.jp>
2024-06-25 15:33:12 +0900

    新しくした！

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        if (! $request->expectsJson()) {
+            // パスがadmin/から始まる場合、管理者ログイン画面へリダイレクト
+            if ($request->is('admin/*')) {
+                return route('admin.create');
+            }
+            return route('login');
+        }            
+    }
+}
